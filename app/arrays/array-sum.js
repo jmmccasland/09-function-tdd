1f9d7ff3704ad9a037ce27b50dba1ad7579e11d7
John Michael McCasland <jmmccasland@gmail.com>
2017-02-13 17:41:12 -0600

    Array Sum Tests pass

--- app/arrays/array-sum.js
+++ app/arrays/array-sum.js
@@ -1,1 +1,10 @@
-export default function () {}
+export default function (array) {
+  const length = array.length;
+  let sumValue = 0;
+
+  for (let i = 0; i < length; i += 1) {
+    sumValue += array[i];
+  }
+
+  return sumValue;
+}
