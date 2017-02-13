e99a422db4d8142dca898d5ee88be46ecf6ddde2
John Michael McCasland <jmmccasland@gmail.com>
2017-02-13 17:30:04 -0600

    sameFirst tests pass

--- app/arrays/same-first.js
+++ app/arrays/same-first.js
@@ -1,1 +1,13 @@
-export default function () {}
+export default function (array1, array2) {
+  if (array1[0] === array2[0]) {
+    const sameFirst = true;
+
+    return sameFirst;
+  }
+
+  if (array1[0] != array2[0]) {
+    const sameFirst = false;
+
+    return sameFirst;
+  }
+}
