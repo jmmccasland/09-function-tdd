d934257e5fc835c82dff2af063e938d677a83212
John Michael McCasland <jmmccasland@gmail.com>
2017-02-13 16:58:42 -0600

    Should Hit Tests Pass

--- app/blackjack/should-hit.js
+++ app/blackjack/should-hit.js
@@ -1,5 +1,14 @@
 import cardSum from './card-sum';
 
-export default function (cardSum) {
+export default function (value1, value2) {
+  if (value1 === value2) {
+    return false;
+  }
+  if (cardSum(value1, value2) <= 17) {
+    return true;
+  }
 
+  if (cardSum(value1, value2) > 17) {
+    return false;
+  }
 }
