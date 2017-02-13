84e09299fc58fe70a06472e7ea40d39dc6f331fb
John Michael McCasland <jmmccasland@gmail.com>
2017-02-13 17:33:25 -0600

    Deep Equal Tests Pass

--- app/arrays/deep-equal.js
+++ app/arrays/deep-equal.js
@@ -1,1 +1,13 @@
-export default function () {}
+export default function (array1, array2) {
+  if (array1.length !=  array2.length) {
+    return false;
+  }
+
+  if (array1[0] === array2[0] && array1[1] === array2[1]) {
+    return true;
+  }
+
+  if (array1[0] != array2[0] || array1[1] != array2[1]) {
+    return false;
+  }
+}
